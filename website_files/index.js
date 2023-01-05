4849a066a40ed4278ba7d6c966453af66b57b8d3
jayasankar-shyam <jayasankar06shyam@gmail.com>
2023-01-05 21:15:04 +0530

    minor

--- website_files/index.js
+++ website_files/index.js
@@ -1,4 +1,4 @@
-const images = ["sample1.png", "9.png", "three.png"];
+const images = ["sample1.png", "sample2.png", "sample3.png"];
 
 const random = Math.floor(Math.random() * images.length);
 console.log(images[random]);
