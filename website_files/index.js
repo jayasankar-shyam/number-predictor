89d9af2e44ac71d423eb1488a0fb64e96262551e
jayasankar-shyam <jayasankar06shyam@gmail.com>
2023-01-05 21:12:21 +0530

    update

---
+++ website_files/index.js
@@ -0,0 +1,8 @@
+const images = ["sample1.png", "9.png", "three.png"];
+
+const random = Math.floor(Math.random() * images.length);
+console.log(images[random]);
+function changeImage() {
+    document.getElementById("d-img").setAttribute('href', "images/"+images[random])
+}
+window.onload = changeImage;
