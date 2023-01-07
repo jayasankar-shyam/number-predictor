1a9a36b44a7664cbd11fcdd249ddd47a0344ab77
jayasankar-shyam <jayasankar06shyam@gmail.com>
2023-01-07 20:54:33 +0530

    update

--- website_files/index.js
+++ website_files/index.js
@@ -3,6 +3,6 @@ const images = ["sample1.png", "sample2.png", "sample3.png"];
 const random = Math.floor(Math.random() * images.length);
 console.log(images[random]);
 function changeImage() {
-    document.getElementById("d-img").setAttribute('href', "images/"+images[random])
+    document.getElementById("d-img").setAttribute('href', "website_files/images/"+images[random])
 }
 window.onload = changeImage;
